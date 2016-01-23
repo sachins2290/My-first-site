6729ec9666dc8d67d532c873455eb0cad47b0394
sachins2290 <sachins2290@gmail.com>
2016-01-22 16:55:41 -0800

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,6 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-<<<<<<< HEAD
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
->>>>>>> 6f62c0ccd198aa2e163328ed3164ed530bd47775
+
